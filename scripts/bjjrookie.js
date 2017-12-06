06ef7781c3a8cf98f493bd1ee3fe7b4932944afd
DaigneaultPearce <Daigneault.Pearce@gmail.com>
2017-12-05 23:58:44 -0800

    COLLAPSEABLE PANELS

--- scripts/bjjrookie.js
+++ scripts/bjjrookie.js
@@ -1,2 +1,22 @@
-var apple = "apple";
-console.log(apple);
+// Creates a panel from an element that contains
+// both an h2 heading and a .content element.
+var makeToggleablePanel = function(panel) {
+    // Select the children of the provided element
+    var control = panel.querySelector("h2"),
+        content = panel.querySelector(".content");
+    
+    // Function that toggles content's visibility via css.
+    var toggleVisibility = function(event) {
+      content.classList.toggle("closed");  
+    }
+    
+    // When control is clicked, call our toggleVisibility function. 
+    control.addEventListener( "click", toggleVisibility );
+  };
+  
+  // Turn everything on the page with class panel into a panel.
+  var panels = document.querySelectorAll(".panel"),
+      i = 0;
+  for( i; i < panels.length; i++ ) {
+    makeToggleablePanel( panels[i] );
+  }
